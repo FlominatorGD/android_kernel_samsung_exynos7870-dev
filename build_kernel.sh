ab857eaa3919be56325418ed20614a9ac09fd398
FlominatorGD <github.flominatorgd@gmail.com>
2024-02-13 23:12:02 +0100

    add a build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export ANDROID_MAJOR_VERSION=q
+export CROSS_COMPILE=aarch64-linux-gnu-
+export PATH=$PWD/toolchain/bin:$PATH
+export ANDROID_MAJOR_VERSION=o
+COMMON="O=out CC=clang LD=ld.lld OBJDUMP=llvm-objdump OBJCOPY=llvm-objcopy AR=llvm-ar AS=llvm-as"
 
-make exynos7870-a6lte_defconfig
-make -j64
+make $COMMON exynos7870-a3y17lte_defconfig
+cp out/.config arch/arm64/configs/exynos7870-a3y17lte_defconfig
+make $COMMON -j$(nproc)
