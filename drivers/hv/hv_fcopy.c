2a36c7d1f2946a17bc57dc6c7257b5cd05c5f5b4
FlominatorGD <github.flominatorgd@gmail.com>
2024-10-22 23:56:50 +0200

    [FROMLIST-EXYNOS7870] [drivers] hv: patch hv_kvp.c
    original base: A600FNXXU9CVB1 rebase: origin/lineage-18.1-ASB-2020-07-05_2020-08-05_3.18
    
    history: 62b9af7b1e3 Drivers: hv: util: Increase the timeout for util services
    01dfbfc76ca Drivers: hv: util: move kvp/vss function declarations to hyperv_vmbus.h
    19a0f902aa1 A600FNXXU9CVB1

--- drivers/hv/hv_fcopy.c
+++ drivers/hv/hv_fcopy.c
@@ -239,6 +239,7 @@ void hv_fcopy_onchannelcallback(void *context)
 
 		fcopy_transaction.active = true;
 		fcopy_transaction.recv_len = recvlen;
+		fcopy_transaction.recv_channel = channel;
 		fcopy_transaction.recv_req_id = requestid;
 		fcopy_transaction.fcopy_msg = fcopy_msg;
 
@@ -394,7 +395,6 @@ static void fcopy_dev_deinit(void)
 int hv_fcopy_init(struct hv_util_service *srv)
 {
 	recv_buffer = srv->recv_buffer;
-	fcopy_transaction.recv_channel = srv->channel;
 
 	/*
 	 * When this driver loads, the user level daemon that
