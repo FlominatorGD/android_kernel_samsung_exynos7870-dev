a7bd9ad9d0fc9113a7d46476b24a8b3277779b48
FlominatorGD <github.flominatorgd@gmail.com>
2024-10-23 00:40:59 +0200

    [FROMLIST-EXYNOS7870] [net] ipv4: netfilter: patch ip_tables.c
    original base: A600FNXXU9CVB1 rebase: origin/lineage-18.1-ASB-2020-07-05_2020-08-05_3.18
    
    history: 19a0f902aa1 A600FNXXU9CVB1

--- net/ipv4/netfilter/arp_tables.c
+++ net/ipv4/netfilter/arp_tables.c
@@ -969,7 +969,6 @@ static int get_entries(struct net *net, struct arpt_get_entries __user *uptr,
 			 sizeof(struct arpt_get_entries) + get.size);
 		return -EINVAL;
 	}
-	get.name[sizeof(get.name) - 1] = '\0';
 
 	t = xt_find_table_lock(net, NFPROTO_ARP, get.name);
 	if (!IS_ERR_OR_NULL(t)) {
@@ -1328,12 +1327,10 @@ static int translate_compat_table(struct xt_table_info **pinfo,
 	if (!newinfo)
 		goto out_unlock;
 
-	memset(newinfo->entries, 0, size);
-
 	newinfo->number = compatr->num_entries;
 	for (i = 0; i < NF_ARP_NUMHOOKS; i++) {
-		newinfo->hook_entry[i] = compatr->hook_entry[i];
-		newinfo->underflow[i] = compatr->underflow[i];
+		newinfo->hook_entry[i] = info->hook_entry[i];
+		newinfo->underflow[i] = info->underflow[i];
 	}
 	entry1 = newinfo->entries[raw_smp_processor_id()];
 	pos = entry1;
@@ -1545,7 +1542,6 @@ static int compat_get_entries(struct net *net,
 			 *len, sizeof(get) + get.size);
 		return -EINVAL;
 	}
-	get.name[sizeof(get.name) - 1] = '\0';
 
 	xt_compat_lock(NFPROTO_ARP);
 	t = xt_find_table_lock(net, NFPROTO_ARP, get.name);
