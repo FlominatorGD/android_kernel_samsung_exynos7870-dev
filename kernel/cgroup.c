12a353a91160601174e96bd42fd95fcf27692dd6
FlominatorGD <github.flominatorgd@gmail.com>
2024-10-23 00:35:56 +0200

    [FROMLIST-EXYNOS7870] [kernel] kernel: patch cpu.c
    original base: A600FNXXU9CVB1 rebase: origin/lineage-18.1-ASB-2020-07-05_2020-08-05_3.18
    
    history: 6ba2d8d869c stable-fixup: hotplug: fix unused function warning
    6707f4d23a3 hotplug: Make register and unregister notifier API symmetric
    19a0f902aa1 A600FNXXU9CVB1

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2956,10 +2956,6 @@ static int cgroup_rename(struct kernfs_node *kn, struct kernfs_node *new_parent,
 	struct cgroup *cgrp = kn->priv;
 	int ret;
 
-	/* do not accept '\n' to prevent making /proc/<pid>/cgroup unparsable */
-	if (strchr(new_name_str, '\n'))
-		return -EINVAL;
-
 	if (kernfs_type(kn) != KERNFS_DIR)
 		return -ENOTDIR;
 	if (kn->parent != new_parent)
@@ -5470,7 +5466,7 @@ static int cgroup_css_links_read(struct seq_file *seq, void *v)
 		struct task_struct *task;
 		int count = 0;
 
-		seq_printf(seq, "css_set %p\n", cset);
+		seq_printf(seq, "css_set %pK\n", cset);
 
 		list_for_each_entry(task, &cset->tasks, cg_list) {
 			if (count++ > MAX_TASKS_SHOWN_PER_CSS)
