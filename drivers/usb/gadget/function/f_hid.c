329d9174b05f4e1cadce845e549dd543bbfff4f5
FlominatorGD <github.flominatorgd@gmail.com>
2024-10-23 00:19:21 +0200

    [FROMLIST-EXYNOS7870] [drivers] usb: gadget: function: patch f_mass_storage.c
    original base: A600FNXXU9CVB1 rebase: origin/lineage-18.1-ASB-2020-07-05_2020-08-05_3.18
    
    history: 19a0f902aa1 A600FNXXU9CVB1

--- drivers/usb/gadget/function/f_hid.c
+++ drivers/usb/gadget/function/f_hid.c
@@ -219,13 +219,18 @@ static ssize_t f_hidg_read(struct file *file, char __user *buffer,
 	 * call, taking into account its current read position.
 	 */
 	if (list->pos == req->actual) {
-		kfree(list);
+ 		kfree(list);
 
 		req->length = hidg->report_length;
 		ret = usb_ep_queue(hidg->out_ep, req, GFP_KERNEL);
 		if (ret < 0) {
 			free_ep_req(hidg->out_ep, req);
-			return ret;
+ 			return ret;
+		}
+	} else {
+
+		if (ret < 0) {
+			free_ep_req(hidg->out_ep, req);
 		}
 	} else {
 		spin_lock_irqsave(&hidg->spinlock, flags);
@@ -476,13 +481,13 @@ static void hidg_disable(struct usb_function *f)
 	usb_ep_disable(hidg->out_ep);
 	hidg->out_ep->driver_data = NULL;
 
-	spin_lock_irqsave(&hidg->spinlock, flags);
+	spin_lock_irqsave(&hidg->read_spinlock, flags);
 	list_for_each_entry_safe(list, next, &hidg->completed_out_req, list) {
 		free_ep_req(hidg->out_ep, list->req);
 		list_del(&list->list);
 		kfree(list);
 	}
-	spin_unlock_irqrestore(&hidg->spinlock, flags);
+	spin_unlock_irqrestore(&hidg->read_spinlock, flags);
 }
 
 static int hidg_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
@@ -526,7 +531,7 @@ static int hidg_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 		}
 		status = usb_ep_enable(hidg->out_ep);
 		if (status < 0) {
-			ERROR(cdev, "Enable OUT endpoint FAILED!\n");
+			ERROR(cdev, "Enable IN endpoint FAILED!\n");
 			goto fail;
 		}
 		hidg->out_ep->driver_data = hidg;
