48e9a44d4f8f3d0ce1476fc671cd242a1d668e25
FlominatorGD <github.flominatorgd@gmail.com>
2024-10-23 22:34:35 +0200

    [FROMLIST-EXYNOS7870] [block] block: patch bio.c
    original base: A600FNXXU9CVB1 rebase: origin/lineage-18.1-ASB-2020-07-05_2020-08-05_3.18
    
    history: 19a0f902aa1 A600FNXXU9CVB1

--- block/bio.c
+++ block/bio.c
@@ -565,9 +565,13 @@ void __bio_clone_fast(struct bio *bio, struct bio *bio_src)
 	 */
 	bio->bi_bdev = bio_src->bi_bdev;
 	bio->bi_flags |= 1 << BIO_CLONED;
+#ifdef CONFIG_JOURNAL_DATA_TAG
+	bio->bi_flags |= bio_src->bi_flags & BIO_JOURNAL_TAG_MASK;
+#endif
 	bio->bi_rw = bio_src->bi_rw;
 	bio->bi_iter = bio_src->bi_iter;
 	bio->bi_io_vec = bio_src->bi_io_vec;
+	bio->bi_flags |= bio_src->bi_flags & 1UL << BIO_BYPASS;
 }
 EXPORT_SYMBOL(__bio_clone_fast);
 
@@ -650,6 +654,10 @@ struct bio *bio_clone_bioset(struct bio *bio_src, gfp_t gfp_mask,
 	bio->bi_rw		= bio_src->bi_rw;
 	bio->bi_iter.bi_sector	= bio_src->bi_iter.bi_sector;
 	bio->bi_iter.bi_size	= bio_src->bi_iter.bi_size;
+#ifdef CONFIG_JOURNAL_DATA_TAG
+	bio->bi_flags |= bio_src->bi_flags & BIO_JOURNAL_TAG_MASK;
+#endif
+	bio->bi_flags |= bio_src->bi_flags & 1UL << BIO_BYPASS;
 
 	if (bio->bi_rw & REQ_DISCARD)
 		goto integrity_clone;
@@ -1290,7 +1298,6 @@ static struct bio *__bio_map_user_iov(struct request_queue *q,
 	struct bio *bio;
 	int cur_page = 0;
 	int ret, offset;
-	struct bio_vec *bvec;
 
 	for (i = 0; i < iov_count; i++) {
 		unsigned long uaddr = (unsigned long)iov[i].iov_base;
@@ -1334,12 +1341,7 @@ static struct bio *__bio_map_user_iov(struct request_queue *q,
 
 		ret = get_user_pages_fast(uaddr, local_nr_pages,
 				write_to_vm, &pages[cur_page]);
-		if (unlikely(ret < local_nr_pages)) {
-			for (j = cur_page; j < page_limit; j++) {
-				if (!pages[j])
-					break;
-				put_page(pages[j]);
-			}
+		if (ret < local_nr_pages) {
 			ret = -EFAULT;
 			goto out_unmap;
 		}
@@ -1394,8 +1396,10 @@ static struct bio *__bio_map_user_iov(struct request_queue *q,
 	return bio;
 
  out_unmap:
-	bio_for_each_segment_all(bvec, bio, j) {
-		put_page(bvec->bv_page);
+	for (i = 0; i < nr_pages; i++) {
+		if(!pages[i])
+			break;
+		page_cache_release(pages[i]);
 	}
  out:
 	kfree(pages);
