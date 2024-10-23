b4051940a5d657ca180581feafe32b642bc4a62f
FlominatorGD <github.flominatorgd@gmail.com>
2024-10-23 23:14:46 +0200

    [FROMLIST-EXYNOS7870] [fs] xfs: patch xfs_log.c
    original base: A600FNXXU9CVB1 rebase: origin/lineage-18.1-ASB-2020-07-05_2020-08-05_3.18
    
    history: 3933a0c869d xfs: fix mount failure crash on invalid iclog memory access
    19a0f902aa1 A600FNXXU9CVB1

--- fs/xfs/xfs_log.c
+++ fs/xfs/xfs_log.c
@@ -3274,6 +3274,8 @@ maybe_sleep:
 		 */
 		if (iclog->ic_state & XLOG_STATE_IOERROR)
 			return -EIO;
+		if (log_flushed)
+			*log_flushed = 1;
 	} else {
 
 no_sleep:
@@ -3381,6 +3383,8 @@ try_again:
 
 				xlog_wait(&iclog->ic_prev->ic_write_wait,
 							&log->l_icloglock);
+				if (log_flushed)
+					*log_flushed = 1;
 				already_slept = 1;
 				goto try_again;
 			}
@@ -3414,6 +3418,9 @@ try_again:
 			 */
 			if (iclog->ic_state & XLOG_STATE_IOERROR)
 				return -EIO;
+
+			if (log_flushed)
+				*log_flushed = 1;
 		} else {		/* just return */
 			spin_unlock(&log->l_icloglock);
 		}
